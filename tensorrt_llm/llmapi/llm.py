2013f7bc479f484c9399a64194df9c59404c2e23
aikitoria <151776613+aikitoria@users.noreply.github.com>
2025-04-30 05:45:49 +0200

    fix: missing args
    Signed-off-by: aikitoria <151776613+aikitoria@users.noreply.github.com>

--- tensorrt_llm/llmapi/llm.py
+++ tensorrt_llm/llmapi/llm.py
@@ -527,7 +527,7 @@ class LLM:
         if self.args.kv_cache_config is not None:
             executor_config.kv_cache_config = PybindMirror.maybe_to_pybind(
                 self.args.kv_cache_config)
-            runtime_defaults = self._pretrained_config.runtime_defaults
+            runtime_defaults = self.args._pretrained_config.runtime_defaults
             if runtime_defaults:
                 executor_config.kv_cache_config.fill_empty_fields_from_runtime_defaults(
                     runtime_defaults)
